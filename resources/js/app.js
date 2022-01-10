47e825ba623b40869f97f50d78af23ffcfff1d27
Ekaitz <ekaitzmcbo@gmail.com>
2022-01-10 08:41:35 +0100

    AEjemplo Sobre Nosotros VueJS

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,9 @@
 require('./bootstrap');
 
 import Vue from 'vue'
+
+// importar componente
+import SobreNosotros from './components/SobreNosotros.vue'
+
+// crear instancia vue
+Vue.component('sobre-nosotros', require('./components/SobreNosotros.vue').default);
