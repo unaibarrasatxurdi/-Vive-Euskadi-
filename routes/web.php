a460faad7471043c5b8ecdaceb7d48ae01da8384
María <magomez294@gmail.com>
2022-01-10 11:59:34 +0100

    Actualizacion fichero admin

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 */
-Route::get('/', function () {
+/* Route::get('/', function () {
     return view('sobre-nosotros');
+}); */
+Route::get('/', function () {
+    return view('comp-admin');
 });
