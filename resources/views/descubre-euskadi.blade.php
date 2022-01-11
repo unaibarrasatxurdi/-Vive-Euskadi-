66e53ba57d2da35ff666c45f8122317490b54f45
Ekaitz <ekaitzmcbo@gmail.com>
2022-01-11 11:40:18 +0100

    Texto centrado

--- resources/views/descubre-euskadi.blade.php
+++ resources/views/descubre-euskadi.blade.php
@@ -26,13 +26,13 @@
                 <div class="card-header w-100 text-white fs-3">
                     Álava
                 </div>
-                <div class="card flex-row ">
+                <div class="card d-flex flex-column d-md-flex flex-md-row align-items-center">
                     <div class="card-image border-0 img-fluid">
                         <img src="/images/Imagenes/alavaDescubre.jpg" alt="Descubre Àlava" class="descubre-img">
                     </div>
                     
                     <div class="card-body-right ms-3">
-                        <p class="card-text text-white">            
+                        <p class="card-text text-center text-white">            
                             Álava  es el territorio vasco que mayores rasgos diferenciales presenta respecto a los otros dos.  Es conocida por la excelente calidad de sus vinos procedentes de Rioja Alavesa.Su capital, Vitoria-Gasteiz, es  sede del Gobierno Vasco, y por tanto capital administrativa de Euskadi.Álava cuenta con importantes espacios naturales, que por la escasa industrialización, se encuentran en buen estado de conservación, como por ejemplo, Gorbeia, Urkiola y Aizkorri.</p>
                         <a href="#" class="card-link float-end me-5">Ver más</a>
                     </div>
@@ -45,13 +45,13 @@
                 <div class="card-header w-100 text-white fs-3">
                     Gipuzcoa
                 </div>
-                <div class="card flex-row">
-                    <div class="card-image border-0 img-fluid">
+                <div class="card d-flex flex-column d-md-flex flex-md-row align-items-center">
+                    <div class="card-image border-0 img-fluid" >
                         <img src="/images/Imagenes/gipuzcoaDescubre.jpg" alt="Descubre Gipuzcoa" class="descubre-img">
                     </div>
                     
                     <div class="card-body-right ms-3">
-                        <p class="card-text text-white">            
+                        <p class="card-text text-center text-white">            
                             Gipuzkoa es un territorio plural en el que cada comarca goza de personalidad propia. Desde el azul del Cantábrico hasta los espacios naturales del interior, pasando por ciudades industriales situadas a escasos kilómetros de comarcas rurales.
                             Es el territorio menos extenso en comparación con Araba y Bizkaia, una tierra de contrastes que ha sabido conjugar el arraigo de sus tradiciones, sin dejar de estar en primera línea de la vanguardia tecnológica, empresarial y artística.
                              La costa de Gipuzkoa, a lo largo de 86 km, cuenta con innumerables atractivos. En el litoral occidental, las hermosas villas costeras de Zarautz, Getaria y Mutriku constituyen deseados destinos vacacionales para muchos turistas.</p>
@@ -67,13 +67,13 @@
                     
                     Vizcaya
                 </div>
-                <div class="card flex-row">
+                <div class="card d-flex flex-column d-md-flex flex-md-row align-items-center">
                     <div class="card-image border-0 img-fluid">
                         <img src="/images/Imagenes/vizcayaDescubre.jpg" alt="Descubre Vizcaya" class="descubre-img">
                     </div>
                     
                     <div class="card-body-right ms-3">
-                        <p class="card-text text-white">            
+                        <p class="card-text text-center text-white">            
                             Bizkaia se distingue principalmente por su avanzada modernidad y por ser una tierra de contrastes 
                             que guarda con mimo sus tradiciones.
                             Es el territorio más poblado de Euskadi y esa aglomeración demográfica se sitúa en torno a Bilbao.
