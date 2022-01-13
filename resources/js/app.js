eb9dcb3a6f34d84ba271414afe9e93592b5a6a9d
María <magomez294@gmail.com>
2022-01-13 12:30:29 +0100

    cambios admin
    -He añadido los componentes de admin
    -Cambios en el diseño de admin

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,6 +7,8 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 // crear instancia vue
 Vue.component('cabecera', require('./components/Cabecera.vue').default);
 Vue.component('sobre-nosotros', require('./components/SobreNosotros.vue').default);
+Vue.component('gestionusuarios-admin', require('./components/gestionUsuarios-admin.vue').default);
+Vue.component('gestioncomentarios-admin', require('./components/gestionComentarios-admin.vue').default);
 
 const app = new Vue({
     el: '#app'
--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -47,20 +47,13 @@
                         </div>
                     </div>
                     <main class="col ps-md-2 pt-2">
-                        <a href="#" data-bs-target="#sidebar" data-bs-toggle="collapse" class="border rounded-3 p-1 text-decoration-none"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+                        <a href="#" data-bs-target="#sidebar" data-bs-toggle="collapse" class="border border-dark rounded-3 py-2 text-decoration-none">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
                             <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
-                          </svg></a>
-                        <div class="page-header mt-5 ms-5 ps-4 pt-1" id="titApartado">
-                                <h2>Gestión de usuarios</h2>
-                        </div>
-                        
-                        <div class="row">
-                            <div class="col-12">
-                                <p>This is a simple collapsing sidebar menu for Bootstrap 5. Unlike the Offcanvas component that overlays the content, this sidebar will "push" the content. Sriracha biodiesel taxidermy organic post-ironic, Intelligentsia salvia mustache 90's code editing brunch. Butcher polaroid VHS art party, hashtag Brooklyn deep v PBR narwhal sustainable mixtape swag wolf squid tote bag. Tote bag cronut semiotics, raw denim deep v taxidermy messenger bag. Tofu YOLO Etsy, direct trade ethical Odd Future jean shorts paleo. Forage Shoreditch tousled aesthetic irony, street art organic Bushwick artisan cliche semiotics ugh synth chillwave meditation. Shabby chic lomo plaid vinyl chambray Vice. Vice sustainable cardigan, Williamsburg master cleanse hella DIY 90's blog.</p>
-                                <p>Ethical Kickstarter PBR asymmetrical lo-fi. Dreamcatcher street art Carles, stumptown gluten-free Kickstarter artisan Wes Anderson wolf pug. Godard sustainable you probably haven't heard of them, vegan farm-to-table Williamsburg slow-carb readymade disrupt deep v. Meggings seitan Wes Anderson semiotics, cliche American Apparel whatever. Helvetica cray plaid, vegan brunch Banksy leggings +1 direct trade. Wayfarers codeply PBR selfies. Banh mi McSweeney's Shoreditch selfies, forage fingerstache food truck occupy YOLO Pitchfork fixie iPhone fanny pack art party Portland.</p>
-                            </div>
-                        </div>
-                    </main>
+                          </svg>
+                        </a>
+                    <gestionusuarios-admin></gestionusuarios-admin>
+                    <gestioncomentarios-admin></gestioncomentarios-admin>
                 </div>
             </div>
                 
