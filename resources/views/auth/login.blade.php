03a0fc58f9d7d39ee2bc67b58b61c8441ce4b862
enekovilamor <57180168+enekovilamor@users.noreply.github.com>
2022-01-17 12:15:38 +0100

    ngbfdv

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,8 +2,7 @@
     <x-auth-card>
         <x-slot name="logo">
             <a href="/">
-                <img src="/images/login1.png" width=50em height=50em/>
-                  <!--<x-application-logo class="w-20 h-20 fill-current text-gray-500" />-->
+                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
             </a>
         </x-slot>
 
@@ -25,7 +24,7 @@
 
             <!-- Password -->
             <div class="mt-4">
-                <x-label for="password" :value="__('Contraseña')" />
+                <x-label for="password" :value="__('Password')" />
 
                 <x-input id="password" class="block mt-1 w-full"
                                 type="password"
@@ -37,19 +36,19 @@
             <div class="block mt-4">
                 <label for="remember_me" class="inline-flex items-center">
                     <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Recordar contraseña') }}</span>
+                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
                 </label>
             </div>
 
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Ha olvidado su contraseña?') }}
+                        {{ __('Forgot your password?') }}
                     </a>
                 @endif
 
                 <x-button class="ml-3">
-                    {{ __('Entrar') }}
+                    {{ __('Log in') }}
                 </x-button>
             </div>
         </form>
