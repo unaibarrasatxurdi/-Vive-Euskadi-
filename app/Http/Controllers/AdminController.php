325089abc4fd80eb23657b7b4983f6e96560afb9
María <magomez294@gmail.com>
2022-01-31 10:54:36 +0100

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -27,6 +27,7 @@ class AdminController extends Controller
 
     public function adminUsuario()
     {
+        /* $users = User::paginate(10); */
         $users = User::all();
         return view('admin.usuAd')->with('users', $users);
     }
