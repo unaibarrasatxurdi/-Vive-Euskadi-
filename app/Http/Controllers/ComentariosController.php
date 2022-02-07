63fd9fa663082529e234eaff31fdd96ab1df86d1
enekovilamor <57180168+enekovilamor@users.noreply.github.com>
2022-02-07 13:15:06 +0100

    Cositas

--- app/Http/Controllers/ComentariosController.php
+++ app/Http/Controllers/ComentariosController.php
@@ -13,6 +13,14 @@ class ComentariosController extends Controller
         // $result = DB::select('select * from comentarios where DocumentName = ?', [$documentName]);
         $result = DB::select('SELECT c.idComentario, c.DocumentName, c.Texto, c.Fecha, u.name FROM comentarios c, users u WHERE DocumentName = ? AND u.id = c.id', [$documentName]);
         return $result;
-        
+    }
+    //Función que hace una inserccion en la tabla de comentarios usando la id del usuario y el plan en el que se ha hecho el comentario
+    public function insertarComentario( $id, $documentname) {
+        DB::insert('INSERT INTO comentarios (id, DocumentName) VALUES (?, ?) ON DUPLICATE KEY UPDATE DocumentName = DocumentName', [$id, $documentname]);
+    }
+
+    //Función que eliminara el comentario recibiendo el id de el comentario a borrar 
+    public function borrarComentario($IdComentario) {
+        DB::delete('DELETE FROM comentarios WHERE IdComentario = ?',[$IdComentario]);
     }
 }
