4b1459490601ee232e94459ce9e15d4616a26b01
María <magomez294@gmail.com>
2022-02-24 17:48:29 +0100

    admin 24/02

--- resources/views/admin/create.blade.php
+++ resources/views/admin/create.blade.php
@@ -8,10 +8,13 @@
    <section id="contNewUser">
       <form action="" method="post">
          
-         <div class="d-flex justify-content-center">
-               <img src="/images/Imagenes/bodyDescubre.jpg" class="img-fluid circular--square" alt="" id="imgUser">
+         <div class="d-flex justify-content-center mb-4">
+               <label for="fotoUsuario">
+                  <img src="/images/Imagenes/login1.png" class="img-fluid circular--square" alt="" id="imgUser">
+               </label>
+               <input type="file" name="" id="fotoUsuario" onchange="previewFile(this);">
         </div>
-        <input type="file" name="" id="">
+        
         <div class="mb-3">
          <input type="text" class="border rounded-pill border-primary shadow form-control" placeholder="Nombre" required autofocus>
       </div>
@@ -25,8 +28,9 @@
          <input type="password" class="border rounded-pill border-primary shadow form-control" placeholder="Repite la contraseña" required autofocus>
       </div>
       <div class="d-md-flex justify-content-md-center align-items-md-center mb-3">
-         <button type="submit" class="btn btn-primary">Crear</button>
          <a href="/admin/gestion-usuarios" class="btn btn-primary mx-2">Volver atrás</a>
+         <button type="submit" class="btn btn-primary">Crear</button>
+         
         
       </div>
       </form>
@@ -34,42 +38,6 @@
 
    </section>
 
- 
-
-
-
-
-
-
-
-
-
-
-   {{-- <section class="border-primary shadow-sm contact-clean">
-      <form method="post">
-         <h2 class="text-center">
-            Nuevo Usuario
-         </h2>
-         <div class="mb-3">
-            <input type="text" class="border rounded-pill border-primary shadow form-control" placeholder="Nombre" required autofocus>
-         </div>
-         <div class="mb-3">
-            <input type="email" class="border rounded-pill border-primary shadow form-control" placeholder="Email" required autofocus>
-         </div>
-         <div class="mb-3">
-            <input type="password" class="border rounded-pill border-primary shadow form-control" placeholder="Contraseña" required autofocus>
-         </div>
-         <div class="mb-3">
-            <input type="password" class="border rounded-pill border-primary shadow form-control" placeholder="Repite la contraseña" required autofocus>
-         </div>
-         <div class="d-md-flex justify-content-md-center align-items-md-center mb-3">
-            <button type="submit" class="btn btn-primary">Crear</button>
-
-         </div>
-      </form>
-
-
-
-   </section> --}}
+   
 </main>
 @endsection
